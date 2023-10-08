df9cf3e868cb3ef3f396f505448b77185fd33825
KINGShinjoon <sj@cnitsstudio.com>
2023-10-08 15:23:40 +0900

    Update VirtualTime.cs

--- 2023.10.08 강의/유니티 스크립트/VirtualTime.cs
+++ 2023.10.08 강의/유니티 스크립트/VirtualTime.cs
@@ -11,14 +11,27 @@ public class VirtualTime : MonoBehaviour
 
     public Text timeText; //시간을 표시하기 위해서 필요
    
-    void Start() //시작되자마자 실행되는 코드
+    private void Start() //시작되자마자 실행되는 코드
     {
         timeText.text = year + "년" + month + "월" + day + "일";
+        StartCoroutine(StartTime());
     }
 
-    // Update is called once per frame
-    void Update()
+   private IEnumerator StartTime()
     {
-        
+        while(true) // while 해당하는 값이 충족할 때 지속적으로 실행
+        {
+            yield return new WaitForSeconds(15); //15초 대기 이후 실행 ㅡ 이후 변경
+            day++; // day 하루 증가함 
+            //day = day+1;
+            //day++
+
+            if(day>30)
+            {
+                day = 1;
+                month++;
+            }
+            timeText.text = year + "년" + month + "월" + day + "일";
+        }
     }
 }
