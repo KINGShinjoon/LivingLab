7fb56b1254cca669be8e238236a364a0f7cc6aea
KINGShinjoon <sj@cnitsstudio.com>
2023-10-29 13:52:01 +0900

    Update GLTFModel.js

--- 2023.10.29 강의/리엑트 스크립트/GLTFModel.js
+++ 2023.10.29 강의/리엑트 스크립트/GLTFModel.js
@@ -1,19 +1,30 @@
-import React, { useRef, useEffect } from 'react';
-import { useLoader } from '@react-three/fiber';
+import React, { useRef, useEffect,useMemo } from 'react';
+import { useLoader,useFrame } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import {AnimationMixer} from 'three';
 
 function GLTFModel({ url }) {
   const gltf = useLoader(GLTFLoader, url);
   const ref = useRef();
-
+  const mixer = useMemo(() => new AnimationMixer(),[]);
   useEffect(() => {
     if (ref.current) {
         ref.current.scale.set(0.1,0.1,0.1);
         ref.current.rotation.y = Math.PI;
       ref.current.position.set(1, -3, -2);
     }
-  }, [ref.current]);
+    if(ref.current && gltf.animations.length>0){
+        const animation = gltf.animations.find 
+        (clip => clip.name === "Idle 01");
+        if(animation){
+            mixer.clipAction(animation,ref.current).play();
+        }else{
+            console.warn("Animation not found");
+        }
+    }
+  }, [ref.current, gltf.animations, mixer]);
 
+  useFrame((state, delta) => mixer.update(delta));
   return <primitive ref={ref} object={gltf.scene} />;
 }
 export default GLTFModel;
